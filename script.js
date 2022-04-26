6536e29a7431032d5680a83fbdf5d3ccd7673571
TyomSoghomonyan <artyomsoghomonyan2000@gmail.com>
2022-04-26 20:02:23 +0400

    CRUD

---
+++ script.js
@@ -0,0 +1,79 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["name"] = document.getElementById("name").value;
+    formData["lastname"] = document.getElementById("lastname").value;
+    formData["age"] = document.getElementById("age").value;
+    formData["salary"] = document.getElementById("salary").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.name;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.lastname;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.age;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.salary;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("name").value = "";
+    document.getElementById("lastname").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("salary").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("name").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("lastname").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("age").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.name;
+    selectedRow.cells[1].innerHTML = formData.lastname;
+    selectedRow.cells[2].innerHTML = formData.age;
+    selectedRow.cells[3].innerHTML = formData.salary;
+}
+
+function onDelete(td) {
+    if (confirm('utrsjgcjhլ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("name").value == "") {
+        isValid = false;
+        document.getElementById("nameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("nameValidationError").classList.contains("hide"))
+            document.getElementById("nameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
